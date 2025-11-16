b9244d99427c1ab69a24d508f35d72212e556a19
kartikey2004-git <kartikeybhatnagar247@gmail.com>
2025-11-16 18:43:15 +0530

    start implementation device flow and build kairo login command flow

---
+++ server/src/cli/main.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+
+import dotenv from "dotenv";
+import chalk from "chalk";
+import figlet from "figlet";
+import { Command } from "commander";
+import { login } from "./commands/auth/login.js";
+
+dotenv.config();
+
+async function main() {
+  // Display banner
+  console.log(
+    chalk.cyan(
+      figlet.textSync("Kairo CLI", {
+        font: "Standard",
+        horizontalLayout: "default",
+      })
+    )
+  );
+
+  console.log(chalk.gray("A CLI based AI tool \n"));
+
+  const program = new Command("kairo");
+
+  program.version("0.0.1").description("Orbital CLI - A CLI based AI Tool").addCommand(login)
+
+  program.action(() => {
+    program.help();
+  });
+
+  program.parse();
+}
+
+main().catch((err) => {
+  console.log(chalk.red("Error running kairo CLI:"), err);
+  process.exit(1);
+});
